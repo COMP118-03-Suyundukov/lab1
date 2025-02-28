a3e8d3d0443916a7395e2987609c66d6ea30005a
Nurlan Suyundukov <hey@suyundukov.com>
2025-02-28 14:12:47 +0200

    Show menu

--- lab1.cpp
+++ lab1.cpp
@@ -17,5 +17,14 @@ void drawRectangleFilled(int, int, char);
 
 // Driver of the application
 int main() {
+	// Show menu
+	cout << "1) Draw a horizontal line" << endl;
+	cout << "2) Draw a vertical line" << endl;
+	cout << "3) Draw a square" << endl;
+	cout << "4) Draw a square filled" << endl;
+	cout << "5) Draw a rectangle" << endl;
+	cout << "6) Draw a rectangle filled" << endl;
+	cout << "7) Quit" << endl;
+
     cout << "Have a nice day ^^" << endl;
 }
