f1c7ad99e1309e3314172e964023c57f39a4cd8a
Student <Student@B111-15>
2025-02-28 13:38:30 +0200

    Add project files.

---
+++ lab1.cpp
@@ -0,0 +1,16 @@
+// lab1.cpp : This file contains small drawing program
+// Date: 28/02/2025
+// Author: Nurlan Suyundukov
+
+#include <iostream>
+
+using namespace std;
+
+// Global variables
+
+// Function prototype
+
+// Driver of the application
+int main() {
+    cout << "Have a nice day ^^" << endl;
+}
