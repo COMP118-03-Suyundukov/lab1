567ffb9f16a829168b8a85e7c5b639f9a85470dd
Nurlan Suyundukov <hey@suyundukov.com>
2025-02-28 14:44:59 +0200

    Added some sort of menu handling

--- lab1.cpp
+++ lab1.cpp
@@ -14,10 +14,54 @@ void drawSquare(int, char);
 void drawSquareFilled(int, char);
 void drawRectangle(int, int, char);
 void drawRectangleFilled(int, int, char);
+void showMenu();
 
 // Driver of the application
 int main() {
-	// Show menu
+	int choice;
+
+	do {
+		// Show menu
+		showMenu();
+
+		// Get user's choice
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+		// Handle user's choice
+		if (choice == 1) {
+
+		}
+		else if (choice == 2) {
+
+		}
+		else if (choice == 3) {
+
+		}
+		else if (choice == 4) {
+
+		}
+		else if (choice == 5) {
+
+		}
+		else if (choice == 6) {
+
+		}
+		else if (choice == 7) {
+			// No code is needed here
+		}
+		else {
+			cerr << "Wrong choice, buddy!" << endl;
+		}
+	} while (choice != 7);
+
+	cout << "Have a nice day, pal, ^^" << endl;
+
+	return 0;
+}
+
+// Show the menu options to the user
+void showMenu() {
 	cout << "1) Draw a horizontal line" << endl;
 	cout << "2) Draw a vertical line" << endl;
 	cout << "3) Draw a square" << endl;
@@ -25,6 +69,4 @@ int main() {
 	cout << "5) Draw a rectangle" << endl;
 	cout << "6) Draw a rectangle filled" << endl;
 	cout << "7) Quit" << endl;
-
-	cout << "Have a nice day ^^" << endl;
 }
