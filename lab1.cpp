0bce7ceb2ab56bdffb76bf4aaf4658e9fadb0ee3
Nurlan Suyundukov <hey@suyundukov.com>
2025-02-28 14:10:55 +0200

    Added function prototypes

--- lab1.cpp
+++ lab1.cpp
@@ -1,7 +1,6 @@
 // lab1.cpp : This file contains small drawing program
 // Date: 28/02/2025
 // Author: Nurlan Suyundukov
-
 #include <iostream>
 
 using namespace std;
@@ -9,6 +8,12 @@ using namespace std;
 // Global variables
 
 // Function prototype
+void drawHorizontalLine(int, char);
+void drawVerticalLine(int, char);
+void drawSquare(int, char);
+void drawSquareFilled(int, char);
+void drawRectangle(int, int, char);
+void drawRectangleFilled(int, int, char);
 
 // Driver of the application
 int main() {
