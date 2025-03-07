8c9d09707c0879663180c50c6dee300083ef7ba7
Nurlan Suyndukov <hey@suyundukov.com>
2025-03-07 12:52:56 +0200

    Added 'drawHorizontalLine' implementation

--- main.cpp
+++ main.cpp
@@ -6,9 +6,10 @@
 using namespace std;
 
 // Global variables
+const char DEFAULT_SYMBOL = 'X';
 
 // Function prototype
-void drawHorizontalLine(int, char);
+void drawHorizontalLine(int, char = DEFAULT_SYMBOL);
 void drawVerticalLine(int, char);
 void drawSquare(int, char);
 void drawSquareFilled(int, char);
@@ -18,48 +19,65 @@ void showMenu();
 
 // Driver of the application
 int main() {
-	int choice;
+    int choice;
 
-	do {
-		// Show menu
-		showMenu();
+    do {
+        // Show menu
+        showMenu();
 
-		// Get user's choice
-		cout << "Enter your choice: ";
-		cin >> choice;
+        // Get user's choice
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-		// Handle user's choice
-		if (choice == 1) {
+        // Handle user's choice
+        if (choice == 1) {
+            // Draw a horizontal line
+            char symbol;
+            int length;
 
-		} else if (choice == 2) {
+            cout << "Give me the length of the line: ";
+            cin >> length;
+            cout << "Give me the symbol: ";
+            cin >> symbol;
 
-		} else if (choice == 3) {
+            drawHorizontalLine(length, symbol);
+        } else if (choice == 2) {
 
-		} else if (choice == 4) {
+        } else if (choice == 3) {
 
-		} else if (choice == 5) {
+        } else if (choice == 4) {
 
-		} else if (choice == 6) {
+        } else if (choice == 5) {
 
-		} else if (choice == 7) {
-			// No code is needed here
-		} else {
-			cerr << "Wrong choice, buddy!" << endl;
-		}
-	} while (choice != 7);
+        } else if (choice == 6) {
 
-	cout << "Have a nice day, pal, ^^" << endl;
+        } else if (choice == 7) {
+            // No code is needed here
+        } else {
+            cerr << "Wrong choice, buddy!" << endl;
+        }
+    } while (choice != 7);
 
-	return 0;
+    cout << "Have a nice day, pal, ^^" << endl;
+
+    return 0;
 }
 
 // Show the menu options to the user
 void showMenu() {
-	cout << "1) Draw a horizontal line" << endl;
-	cout << "2) Draw a vertical line" << endl;
-	cout << "3) Draw a square" << endl;
-	cout << "4) Draw a square filled" << endl;
-	cout << "5) Draw a rectangle" << endl;
-	cout << "6) Draw a rectangle filled" << endl;
-	cout << "7) Quit" << endl;
+    cout << "1) Draw a horizontal line" << endl;
+    cout << "2) Draw a vertical line" << endl;
+    cout << "3) Draw a square" << endl;
+    cout << "4) Draw a square filled" << endl;
+    cout << "5) Draw a rectangle" << endl;
+    cout << "6) Draw a rectangle filled" << endl;
+    cout << "7) Quit" << endl;
+}
+
+// Draw a horizontal line
+void drawHorizontalLine(int length, char symbol) {
+    for (int i = 0; i < length; i++) {
+        cout << symbol;
+    }
+    cout << endl;
 }
